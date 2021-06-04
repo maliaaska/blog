ff8b9b735158e3078f38725a1857402b28012de4
Arkadiusz Milewski <asmilewski@gmail.com>
2021-06-04 18:38:11 +0200

    the name of the user correctly displayed under the comment

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import postsReducers from "./postsReducers";
+import usersReducers from "./UsersReducers";
 
 export default combineReducers({
   posts: postsReducers,
+  users: usersReducers,
 });
